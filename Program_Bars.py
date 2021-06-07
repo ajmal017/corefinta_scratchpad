ee1f2cfe8e882049db88a398dbcb7c484188caa5
ebtrader <jsiddique@gmail.com>
2021-06-06 22:27:07 -0400

    Commit

---
+++ Program_Bars.py
@@ -0,0 +1,417 @@
+"""
+Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
+"""
+
+import argparse
+import datetime
+import collections
+import inspect
+
+import logging
+import time
+import os.path
+
+from ibapi import wrapper
+from ibapi import utils
+from ibapi.client import EClient
+from ibapi.utils import iswrapper
+
+# types
+from ibapi.common import *  # @UnusedWildImport
+from ibapi.order_condition import *  # @UnusedWildImport
+from ibapi.contract import *  # @UnusedWildImport
+from ibapi.order import *  # @UnusedWildImport
+from ibapi.order_state import *  # @UnusedWildImport
+from ibapi.execution import Execution
+from ibapi.execution import ExecutionFilter
+from ibapi.commission_report import CommissionReport
+from ibapi.ticktype import *  # @UnusedWildImport
+from ibapi.tag_value import TagValue
+
+from ibapi.account_summary_tags import *
+
+from ContractSamples import ContractSamples
+
+
+def SetupLogger():
+    if not os.path.exists("log"):
+        os.makedirs("log")
+
+    time.strftime("pyibapi.%Y%m%d_%H%M%S.log")
+
+    recfmt = '(%(threadName)s) %(asctime)s.%(msecs)03d %(levelname)s %(filename)s:%(lineno)d %(message)s'
+
+    timefmt = '%y%m%d_%H:%M:%S'
+
+    # logging.basicConfig( level=logging.DEBUG,
+    #                    format=recfmt, datefmt=timefmt)
+    logging.basicConfig(filename=time.strftime("log/pyibapi.%y%m%d_%H%M%S.log"),
+                        filemode="w",
+                        level=logging.INFO,
+                        format=recfmt, datefmt=timefmt)
+    logger = logging.getLogger()
+    console = logging.StreamHandler()
+    console.setLevel(logging.ERROR)
+    logger.addHandler(console)
+
+
+def printWhenExecuting(fn):
+    def fn2(self):
+        print("   doing", fn.__name__)
+        fn(self)
+        print("   done w/", fn.__name__)
+
+    return fn2
+
+
+def printinstance(inst: Object):
+    attrs = vars(inst)
+    print(', '.join("%s: %s" % item for item in attrs.items()))
+
+
+class Activity(Object):
+    def __init__(self, reqMsgId, ansMsgId, ansEndMsgId, reqId):
+        self.reqMsdId = reqMsgId
+        self.ansMsgId = ansMsgId
+        self.ansEndMsgId = ansEndMsgId
+        self.reqId = reqId
+
+
+class RequestMgr(Object):
+    def __init__(self):
+        # I will keep this simple even if slower for now: only one list of
+        # requests finding will be done by linear search
+        self.requests = []
+
+    def addReq(self, req):
+        self.requests.append(req)
+
+    def receivedMsg(self, msg):
+        pass
+
+
+# ! [socket_declare]
+class TestClient(EClient):
+    def __init__(self, wrapper):
+        EClient.__init__(self, wrapper)
+        # ! [socket_declare]
+
+        # how many times a method is called to see test coverage
+        self.clntMeth2callCount = collections.defaultdict(int)
+        self.clntMeth2reqIdIdx = collections.defaultdict(lambda: -1)
+        self.reqId2nReq = collections.defaultdict(int)
+        self.setupDetectReqId()
+
+    def countReqId(self, methName, fn):
+        def countReqId_(*args, **kwargs):
+            self.clntMeth2callCount[methName] += 1
+            idx = self.clntMeth2reqIdIdx[methName]
+            if idx >= 0:
+                sign = -1 if 'cancel' in methName else 1
+                self.reqId2nReq[sign * args[idx]] += 1
+            return fn(*args, **kwargs)
+
+        return countReqId_
+
+    def setupDetectReqId(self):
+
+        methods = inspect.getmembers(EClient, inspect.isfunction)
+        for (methName, meth) in methods:
+            if methName != "send_msg":
+                # don't screw up the nice automated logging in the send_msg()
+                self.clntMeth2callCount[methName] = 0
+                # logging.debug("meth %s", name)
+                sig = inspect.signature(meth)
+                for (idx, pnameNparam) in enumerate(sig.parameters.items()):
+                    (paramName, param) = pnameNparam  # @UnusedVariable
+                    if paramName == "reqId":
+                        self.clntMeth2reqIdIdx[methName] = idx
+
+                setattr(TestClient, methName, self.countReqId(methName, meth))
+
+                # print("TestClient.clntMeth2reqIdIdx", self.clntMeth2reqIdIdx)
+
+
+# ! [ewrapperimpl]
+class TestWrapper(wrapper.EWrapper):
+    # ! [ewrapperimpl]
+    def __init__(self):
+        wrapper.EWrapper.__init__(self)
+
+        self.wrapMeth2callCount = collections.defaultdict(int)
+        self.wrapMeth2reqIdIdx = collections.defaultdict(lambda: -1)
+        self.reqId2nAns = collections.defaultdict(int)
+        self.setupDetectWrapperReqId()
+
+    # TODO: see how to factor this out !!
+
+    def countWrapReqId(self, methName, fn):
+        def countWrapReqId_(*args, **kwargs):
+            self.wrapMeth2callCount[methName] += 1
+            idx = self.wrapMeth2reqIdIdx[methName]
+            if idx >= 0:
+                self.reqId2nAns[args[idx]] += 1
+            return fn(*args, **kwargs)
+
+        return countWrapReqId_
+
+    def setupDetectWrapperReqId(self):
+
+        methods = inspect.getmembers(wrapper.EWrapper, inspect.isfunction)
+        for (methName, meth) in methods:
+            self.wrapMeth2callCount[methName] = 0
+            # logging.debug("meth %s", name)
+            sig = inspect.signature(meth)
+            for (idx, pnameNparam) in enumerate(sig.parameters.items()):
+                (paramName, param) = pnameNparam  # @UnusedVariable
+                # we want to count the errors as 'error' not 'answer'
+                if 'error' not in methName and paramName == "reqId":
+                    self.wrapMeth2reqIdIdx[methName] = idx
+
+            setattr(TestWrapper, methName, self.countWrapReqId(methName, meth))
+
+            # print("TestClient.wrapMeth2reqIdIdx", self.wrapMeth2reqIdIdx)
+
+
+# this is here for documentation generation
+"""
+#! [ereader]
+        # You don't need to run this in your code!
+        self.reader = reader.EReader(self.conn, self.msg_queue)
+        self.reader.start()   # start thread
+#! [ereader]
+"""
+
+
+# ! [socket_init]
+class TestApp(TestWrapper, TestClient):
+    def __init__(self):
+        TestWrapper.__init__(self)
+        TestClient.__init__(self, wrapper=self)
+        # ! [socket_init]
+        self.nKeybInt = 0
+        self.started = False
+        self.nextValidOrderId = None
+        self.permId2ord = {}
+        self.reqId2nErr = collections.defaultdict(int)
+        self.globalCancelOnly = False
+        self.simplePlaceOid = None
+
+    def dumpTestCoverageSituation(self):
+        for clntMeth in sorted(self.clntMeth2callCount.keys()):
+            logging.debug("ClntMeth: %-30s %6d" % (clntMeth,
+                                                   self.clntMeth2callCount[clntMeth]))
+
+        for wrapMeth in sorted(self.wrapMeth2callCount.keys()):
+            logging.debug("WrapMeth: %-30s %6d" % (wrapMeth,
+                                                   self.wrapMeth2callCount[wrapMeth]))
+
+    def dumpReqAnsErrSituation(self):
+        logging.debug("%s\t%s\t%s\t%s" % ("ReqId", "#Req", "#Ans", "#Err"))
+        for reqId in sorted(self.reqId2nReq.keys()):
+            nReq = self.reqId2nReq.get(reqId, 0)
+            nAns = self.reqId2nAns.get(reqId, 0)
+            nErr = self.reqId2nErr.get(reqId, 0)
+            logging.debug("%d\t%d\t%s\t%d" % (reqId, nReq, nAns, nErr))
+
+    @iswrapper
+    # ! [connectack]
+    def connectAck(self):
+        if self.asynchronous:
+            self.startApi()
+
+    # ! [connectack]
+
+    @iswrapper
+    # ! [nextvalidid]
+    def nextValidId(self, orderId: int):
+        super().nextValidId(orderId)
+
+        logging.debug("setting nextValidOrderId: %d", orderId)
+        self.nextValidOrderId = orderId
+        print("NextValidId:", orderId)
+        # ! [nextvalidid]
+
+        # we can start now
+        self.start()
+
+    def start(self):
+        if self.started:
+            return
+
+        self.started = True
+
+        if self.globalCancelOnly:
+            print("Executing GlobalCancel only")
+            self.reqGlobalCancel()
+        else:
+            print("Executing requests")
+            # self.reqGlobalCancel()
+            # self.marketDataTypeOperations()
+            # self.accountOperations_req()
+            # self.tickDataOperations_req()
+            # self.marketDepthOperations_req()
+            self.realTimeBarsOperations_req()
+            # self.historicalDataOperations_req()
+            # self.optionsOperations_req()
+            # self.marketScannersOperations_req()
+            # self.fundamentalsOperations_req()
+            # self.bulletinsOperations_req()
+            # self.contractOperations()
+            # self.newsOperations_req()
+            # self.miscelaneousOperations()
+            # self.linkingOperations()
+            # self.financialAdvisorOperations()
+            # self.orderOperations_req()
+            # self.rerouteCFDOperations()
+            # self.marketRuleOperations()
+            # self.pnlOperations_req()
+            # self.histogramOperations_req()
+            # self.continuousFuturesOperations_req()
+            # self.historicalTicksOperations()
+            # self.tickByTickOperations_req()
+            # self.whatIfOrderOperations()
+
+            print("Executing requests ... finished")
+
+    def keyboardInterrupt(self):
+        self.nKeybInt += 1
+        if self.nKeybInt == 1:
+            self.stop()
+        else:
+            print("Finishing test")
+            self.done = True
+
+    def stop(self):
+        print("Executing cancels")
+        # self.orderOperations_cancel()
+        # self.accountOperations_cancel()
+        # self.tickDataOperations_cancel()
+        self.marketDepthOperations_cancel()
+        # self.realTimeBarsOperations_cancel()
+        # self.historicalDataOperations_cancel()
+        # self.optionsOperations_cancel()
+        # self.marketScanners_cancel()
+        # self.fundamentalsOperations_cancel()
+        # self.bulletinsOperations_cancel()
+        # self.newsOperations_cancel()
+        # self.pnlOperations_cancel()
+        # self.histogramOperations_cancel()
+        # self.continuousFuturesOperations_cancel()
+        # self.tickByTickOperations_cancel()
+        print("Executing cancels ... finished")
+
+    def nextOrderId(self):
+        oid = self.nextValidOrderId
+        self.nextValidOrderId += 1
+        return oid
+
+    @iswrapper
+    # ! [error]
+    def error(self, reqId: TickerId, errorCode: int, errorString: str):
+        super().error(reqId, errorCode, errorString)
+        print("Error. Id:", reqId, "Code:", errorCode, "Msg:", errorString)
+
+    # ! [error] self.reqId2nErr[reqId] += 1
+
+    @iswrapper
+    def winError(self, text: str, lastError: int):
+        super().winError(text, lastError)
+
+    @printWhenExecuting
+    def realTimeBarsOperations_req(self):
+        # Requesting real time bars
+        # ! [reqrealtimebars]
+        eurusd_contract = Contract()
+        eurusd_contract.symbol = 'NQ'
+        eurusd_contract.secType = 'FUT'
+        eurusd_contract.exchange = 'GLOBEX'
+        eurusd_contract.currency = 'USD'
+        eurusd_contract.lastTradeDateOrContractMonth = "202106"
+        # self.reqRealTimeBars(3001, ContractSamples.EurGbpFx(), 5, "MIDPOINT", True, [])
+        self.reqRealTimeBars(3001, eurusd_contract, 5, "TRADES", False, [])
+        # ! [reqrealtimebars]
+
+    @iswrapper
+    # ! [realtimebar]
+    def realtimeBar(self, reqId: TickerId, time: int, open_: float, high: float, low: float, close: float,
+                    volume: int, wap: float, count: int):
+        super().realtimeBar(reqId, time, open_, high, low, close, volume, wap, count)
+        print("RealTimeBar. TickerId:", reqId, RealTimeBar(time, -1, open_, high, low, close, volume, wap, count))
+
+    # ! [realtimebar]
+
+    @printWhenExecuting
+    def realTimeBarsOperations_cancel(self):
+        # Canceling real time bars
+        # ! [cancelrealtimebars]
+        self.cancelRealTimeBars(3001)
+        # ! [cancelrealtimebars]
+
+
+
+
+
+def main():
+    SetupLogger()
+    logging.debug("now is %s", datetime.datetime.now())
+    logging.getLogger().setLevel(logging.ERROR)
+
+    cmdLineParser = argparse.ArgumentParser("api tests")
+    # cmdLineParser.add_option("-c", action="store_True", dest="use_cache", default = False, help = "use the cache")
+    # cmdLineParser.add_option("-f", action="store", type="string", dest="file", default="", help="the input file")
+    cmdLineParser.add_argument("-p", "--port", action="store", type=int,
+                               dest="port", default=7497, help="The TCP port to use")
+    cmdLineParser.add_argument("-C", "--global-cancel", action="store_true",
+                               dest="global_cancel", default=False,
+                               help="whether to trigger a globalCancel req")
+    args = cmdLineParser.parse_args()
+    print("Using args", args)
+    logging.debug("Using args %s", args)
+    # print(args)
+
+    # enable logging when member vars are assigned
+    from ibapi import utils
+    Order.__setattr__ = utils.setattr_log
+    Contract.__setattr__ = utils.setattr_log
+    DeltaNeutralContract.__setattr__ = utils.setattr_log
+    TagValue.__setattr__ = utils.setattr_log
+    TimeCondition.__setattr__ = utils.setattr_log
+    ExecutionCondition.__setattr__ = utils.setattr_log
+    MarginCondition.__setattr__ = utils.setattr_log
+    PriceCondition.__setattr__ = utils.setattr_log
+    PercentChangeCondition.__setattr__ = utils.setattr_log
+    VolumeCondition.__setattr__ = utils.setattr_log
+
+    # from inspect import signature as sig
+    # import code code.interact(local=dict(globals(), **locals()))
+    # sys.exit(1)
+
+    # tc = TestClient(None)
+    # tc.reqMktData(1101, ContractSamples.USStockAtSmart(), "", False, None)
+    # print(tc.reqId2nReq)
+    # sys.exit(1)
+
+    try:
+        app = TestApp()
+        if args.global_cancel:
+            app.globalCancelOnly = True
+        # ! [connect]
+        app.connect("127.0.0.1", args.port, clientId=21)
+        # ! [connect]
+        print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
+                                                      app.twsConnectionTime()))
+
+        # ! [clientrun]
+        app.run()
+        # ! [clientrun]
+    except:
+        raise
+    finally:
+        app.dumpTestCoverageSituation()
+        app.dumpReqAnsErrSituation()
+
+
+if __name__ == "__main__":
+    main()
