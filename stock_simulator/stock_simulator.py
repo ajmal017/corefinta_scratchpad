f8896f938f9771f91d9d3e281a4b5dd9668b8687
ebtrader <jsiddique@gmail.com>
2021-06-19 16:08:02 -0400

    Commit

--- stock_simulator/stock_simulator.py
+++ stock_simulator/stock_simulator.py
@@ -1,5 +1,7 @@
 import time
 import random
+from finta import TA
+import pandas as pd
 
 # use outlook app and put it in favorites by selecting the sender email address and clicking star in upper-right
 # then set notifications for favorites
@@ -11,6 +13,8 @@ class StockSimulator:
         self.buy_msg = "Buy today"
         self.stock_price = 0
         self.stock_list = []
+        self.wma = 0
+        self.signal = "NONE"
 
     def simulator(self):
         while self.counter < 10:
@@ -22,6 +26,15 @@ class StockSimulator:
             print(self.stock_list)
             # self.simple_trigger()
             self.sma()
+            prev_wma = self.wma
+            self.finta_sma()
+            if prev_wma != 0:
+                if self.wma > prev_wma:
+                    self.signal = "LONG"
+                elif self.wma < prev_wma:
+                    self.signal = "SHORT"
+            print(self.signal)
+
             time.sleep(5)
             self.counter += 1
 
@@ -33,6 +46,16 @@ class StockSimulator:
         simple_ma = sum(self.stock_list) / len(self.stock_list)
         formatted_simplema = "{:.2f}".format(simple_ma)
         print(formatted_simplema)
+
+    def finta_sma(self):
+        df = pd.DataFrame()
+        df['open'] = self.stock_list
+        df['high'] = self.stock_list
+        df['low'] = self.stock_list
+        df['close'] = self. stock_list
+        df['sma'] = TA.WMA(df, len(self.stock_list))
+        self.wma = df['sma'].iloc[-1]
+        print(self.wma)
 
     def trigger(self):
         x = 0
