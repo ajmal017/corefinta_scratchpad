e4fe8532fb0996ae3c0b08807b183f4710413f3d
ebtrader <jsiddique@gmail.com>
2021-07-04 17:41:56 -0400

    Commit

--- forecasted_weekly_candle/projected_scratch.py
+++ forecasted_weekly_candle/projected_scratch.py
@@ -21,7 +21,7 @@ ticker = "NQ=F"
 #todays_date = datetime.today().strftime('%Y-%m-%d')
 #print(todays_date)
 #data = yf.download(tickers = ticker, start='2020-01-04', end=todays_date, interval = '1d')
-data = yf.download(tickers = ticker, start='2020-01-04', end='2020-07-02', interval = '1d')
+data = yf.download(tickers = ticker, start='2020-01-04', end='2021-07-02', interval = '1d')
 
 # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
 # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
