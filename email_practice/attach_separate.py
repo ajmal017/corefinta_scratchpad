a82dde0c855f558dc444aaac92e51a7f5d81b7dd
ebtrader <jsiddique@gmail.com>
2021-07-06 11:08:47 -0400

    Commit

--- email_practice/attach_separate.py
+++ email_practice/attach_separate.py
@@ -1,57 +1,53 @@
 import smtplib, ssl
 import time
-import yfinance as yf
-import pandas as pd
-import df_class
+
+import df_class_attach
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 # use outlook app and put it in favorites by selecting the sender email address and clicking star in upper-right
 # then set notifications for favorites
 
 class EmailYahoo:
 
-
-
     def __init__(self):
         self.counter = 0
         self.buy_msg = "Buy today"
-        self.df_short_table = df_class.Sample_DF_Call()
+        self.df_short_table = df_class_attach.Sample_DF_Call()
 
     def email_func(self):
-        port = 587  # For starttls
+        fname = 'selected.csv'
+        port = 465  # For SSL
         smtp_server = "smtp.gmail.com"
-        sender_email = "jmzakatees@gmail.com"
-        receiver_email = 'crudedecay@gmail.com'
-        password = 'xxx'
+        sender_email = "jmzakatees@gmail.com"  # Enter your address
+        receiver_email = "crudedecay@gmail.com"
+        subj = "testing attachments"
+        body = "This is an email with attachment sent from Python"
 
-        message = f'''\
-        From: Javed Siddique {sender_email}
-        To: Javed Siddique {receiver_email}
-        Subject: {self.buy_msg}
+        with open(r"/Users/jsidd/Documents/Quant Library/test.txt",
+                  "r") as file1:
+            passwd = file1.read()
+        password = passwd
 
+        message = MIMEMultipart()
+        message["Subject"] = subj
+        message["From"] = sender_email
+        message["To"] = receiver_email
 
-        Dear Javed, This is the future:\
-        {self.df_short_table.select_to_end}
+        # Add attachment to message and convert message to string
+        with open(fname, 'rb') as file:
+            # Attach the file with filename to the email
+            message.attach(MIMEApplication(file.read(), Name=fname))
 
-        '''
+        # message.attach(part)
+        text = message.as_string()
 
-        # send email here
-
-        # Create a secure SSL context
+        # Log in to server using secure context and send email
         context = ssl.create_default_context()
-
-        # Try to log in to server and send email
-        try:
-            server = smtplib.SMTP(smtp_server, port)
-            server.ehlo()  # Can be omitted
-            server.starttls(context=context)  # Secure the connection
-            server.ehlo()  # Can be omitted
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
-        except Exception as e:
-            print('It did not work!')
-            print(e)
-        finally:
-            server.quit()
+            server.sendmail(sender_email, receiver_email, text)
 
     def trigger(self):
         x = 0
